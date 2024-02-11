6d1f1c931255bc63acfe9583047d5369a9246546
workwithraymond <workwithraymond01@gmail.com>
2024-02-11 15:52:11 -0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import ProjectsCard from "./components/ProjectsCard";
+import Projects from "./components/Projects";
+
 import Skills from "./components/Skills";
 
 const App = () => {
@@ -11,7 +12,8 @@ const App = () => {
    <Hero/>
    <Skills/>
    <About/>
-   <ProjectsCard/>
+
+   <Projects/>
    </>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,21 @@
 import aboutSvg from '../assets/about.svg';
 import SectionTitle from './SectionTitle';
-
 const About = () => {
   return (
-    <section className='bg-white py-20 ' id='about'>
-        <div className='align-element grid md:grid-col-2 items-center gap-16'>
+    <section className='bg-white py-20' id='about'>
+      <div className='align-element grid md:grid-cols-2 items-center gap-16'>
         <img src={aboutSvg} className='w-full h-64' />
         <article>
-            <SectionTitle text='code and coffee' />
-            <p className='text-slate-600 mt-8 leading-loose'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
-            omnis exercitationem tempora, aliquid deleniti tenetur vero esse
-            praesentium eaque dicta fugiat? Molestiae expedita, nulla neque
-            error porro sint distinctio possimus!
-            </p>
+          <SectionTitle text='code and coffee' />
+          <p className='text-slate-600 mt-8 leading-loose'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
+            maiores itaque doloribus asperiores dolorum quas voluptates
+            distinctio, porro beatae quam provident libero animi totam,
+            praesentium voluptatem corrupti dignissimos ipsum accusantium!
+          </p>
         </article>
-        </div>
-    
+      </div>
     </section>
-  )
-}
-export default About
+  );
+};
+export default About;
